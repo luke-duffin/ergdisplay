c344a4d58a35a8a2ab753878980eff389d446543
luke-duffin <luke.duffin@digital.cabinet-office.gov.uk>
2013-09-23 16:14:25 +0100

    Create feedjava.js

---
+++ feedjava.js
@@ -0,0 +1,11 @@
+//Vairables
+
+//call python and get URL list as string
+
+//parse URL list to array
+
+//load URL in DOM? and on success load body into html
+
+//cycle through URLs every 60 seconds
+
+//
